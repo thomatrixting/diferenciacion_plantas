4b8c2a8d50af0158716934ac7090764e7f931448
thomas rodrigues <thomas.quamtum@gmail.com>
2021-08-11 22:43:03 -0500

    puse un cilo while y los continue ademas de mas comentarios para hacer mas amigable la experiencia para el usuario

--- diferecicion_plantas.py
+++ diferecicion_plantas.py
@@ -5,81 +5,98 @@ def preg (text):
   elif rta.startswith("n"):
         return False 
   else:
-        print(f"valor inbalido \"{text}\" intete otra vez")
-        preg(text)
+        print(f"valor inbalido \"{rta}\" intete otra vez")
+        return preg(text)
 
-ovada = preg("la forma es ovada")
-if ovada:
-    entera = preg("el mages es entero")
-    if entera:
-        hoja_compuesta = preg("es una hoja compuesta")
-        if hoja_compuesta:
-            print("es un fresno comun")
-        else:
-            print("es una haya faya jaya")
-    elif not entera:
-        
-        if preg("el magen es dentado"):
+print("""bienbenido a diferenciacion plantas responde
+como te indica para obtener el tipo de planta""")
 
-            print("es una aladierno aladierna")
-        elif preg("es de mahen lobado"):
-
-            print("es un araoble albar carbuyu montes")
-
-        elif preg("el magen es espinozo"):
-            print("es un acebo acebo carrasco,xardon")
-elif not ovada:
-    lineal = preg("es de forma lineal")
-    if lineal:
-        compuesta = preg("es una hoja compuesta")
-        if compuesta:
-            paripinado = preg("la hoja es paripinada")
-            if paripinado:
-                print("es tejo texu, teixo")
-            elif not paripinado:
-                print("es una serbal silvestre capudre alcafrenu")
-        elif not compuesta:
-            entero = preg("es de magen entero")
-            if entero:
-                print("es una madroño yerbode, borranchinal")
-            elif not entero:
-                print("es una laurel llereu")
-    elif not lineal:
-        if preg("es de forma cuneada"):
-            if preg("es entera"):
-                print("es salguera negra salguera")
+while True:
+    print("starting".center(20,"-"))
+    ovada = preg("la forma es ovada")
+    if ovada:
+        entera = preg("el mages es entero")
+        if entera:
+            hoja_compuesta = preg("es una hoja compuesta")
+            if hoja_compuesta:
+                print("es un fresno comun")
+                continue
             else:
-                print("es una carbayo carbayu")
+                print("es una haya faya jaya")
+                continue
+        elif not entera:
+            
+            if preg("el magen es dentado"):
 
-        elif preg("es de forma obicular"):
-            if preg("es lobulado"):
-                print("also humerus")
+                print("es una aladierno aladierna")
+                continue
+            elif preg("es de mahen lobado"):
 
-            elif preg("es de magen espinozo"):
-                print("es una avellano ablanu")
+                print("es un araoble albar carbuyu montes")
+                continue
 
-        elif preg("es de forma obtusa"):
-            print("es una abedul bedul, abedurlu")
+            elif preg("el magen es espinozo"):
+                print("es un acebo acebo carrasco,xardon")
+                continue
+    elif not ovada:
+        lineal = preg("es de forma lineal")
 
-        elif preg("es de forma palmeada"):
-            print("es una espinera blesca")
-        
-        elif preg("es de forma triangular"):
-            print("es una hoja grande tiar tilero teja")
+        if lineal:
+            compuesta = preg("es una hoja compuesta")
+            if compuesta:
+                paripinado = preg("la hoja es paripinada")
+                if paripinado:
+                    print("es tejo texu, teixo")
+                    continue
+                elif not paripinado:
+                    print("es una serbal silvestre capudre alcafrenu")
+                    continue
+            elif not compuesta:
+                entero = preg("es de magen entero")
+                if entero:
+                    print("es una madroño yerbode, borranchinal")
+                    continue
+                elif not entero:
+                    print("es una laurel llereu")
+                    continue
+        elif not lineal:
+            if preg("es de forma cuneada"):
+                if preg("es entera"):
+                    print("es salguera negra salguera")
+                    continue
+                else:
+                    print("es una carbayo carbayu")
+                    continue
 
-        elif preg("es de forma cordada"):
-            print("es una carraca ancina")
+            elif preg("es de forma obicular"):
+                if preg("es lobulado"):
+                    print("also humerus")
+                    continue
 
-        elif preg("es de forma eliptica"):
-            print("sauco xubugo,benito")
+                elif preg("es de magen espinozo"):
+                    print("es una avellano ablanu")
+                    continue
 
-        elif preg("es labulada"):
-            print("es una rebollo rebolu sanplego")
+            elif preg("es de forma obtusa"):
+                print("es una abedul bedul, abedurlu")
+                continue
+            elif preg("es de forma palmeada"):
+                print("es una espinera blesca")
+                continue
+                
+            elif preg("es de forma triangular"):
+                print("es una hoja grande tiar tilero teja")
+                continue
 
+            elif preg("es de forma cordada"):
+                print("es una carraca ancina")
+                continue
 
+            elif preg("es de forma eliptica"):
+                print("sauco xubugo,benito")
+                continue
 
-
-
-        
-
-        
+            elif preg("es labulada"):
+                print("es una rebollo rebolu sanplego")
+                continue
+     
